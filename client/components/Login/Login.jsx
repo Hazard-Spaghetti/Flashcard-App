20000d733ca5b7d90fbfde233dc67fa6ebff68e8
Joseph-McGarry <jrmcgarry88@gmail.com>
2024-01-24 18:25:50 -0500

    kinda working, login renders App but still stays on page
    Co-authored-by: Caleb <calebbelkin@gmail.com>

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -1,12 +1,15 @@
 import App from "../../App.jsx";
 import React from 'react';
 import { useState, useRef } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect, Routes, Navigate } from 'react-router-dom';
 import '../../login.css';
+
 
 const LoginPage = () => {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
     const [loggedIn, setLoggedIn] = useState(false);
+   
 
     // function that occurs on an event, specifically here on the click of the submit button
     const handleClick = async (e) => {
@@ -37,6 +40,7 @@ const LoginPage = () => {
           console.log(userInfo)
           if (userInfo.isLoggedIn) {
               setLoggedIn(true);
+            //   window.location.href = "url redirect to success page";
               console.log("Operation successful:", userInfo.message);
           } else {
               console.error("Operation failed:", userInfo.message);
@@ -46,6 +50,11 @@ const LoginPage = () => {
       }
   };
   return (
+    <div>
+        <Routes>
+        {loggedIn ? <Route path="*" element={<App />} /> : null}
+        </Routes>
+
     <div className='container'>
         <h1 className="welcome">Welcome to Cooler Cards</h1>
         <div className="loginpage">
@@ -58,9 +67,20 @@ const LoginPage = () => {
 
                 <input className="loginbutton" type="submit" /> 
             </form>
-            {loggedIn ? <App /> : null}
         </div>
+    </div>
+    
+        
     </div>
   );
   }
 export default LoginPage;
+{/* <Routes>
+    
+                <Route path='/' element={<App />}></Route>
+            </Routes> */}
+
+
+{/* <Routes>
+                {loggedIn && <Route path="/app" element={<App />}/>}
+            </Routes> */}
